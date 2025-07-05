85dc9f8859500e9dec0937880607c201f79f3908
BrianSuhit <braisuhit@gmail.com>
2025-07-05 16:32:48 -0300

    finalizo menu hamburguesa

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// =====================================
+// LÓGICA DE LA ANIMACIÓN DE SCROLL (Fade-in)
+// =====================================
 const elementosOcultos = document.querySelectorAll('.hidden');
 
 const observador = new IntersectionObserver((entradas) => 
@@ -12,3 +15,30 @@ const observador = new IntersectionObserver((entradas) =>
     });
 
 elementosOcultos.forEach((el) => observador.observe(el));
+
+
+// =====================================
+// LÓGICA DEL MENÚ DE HAMBURGUESA
+// =====================================
+const toggleButton = document.querySelector('.header__toggle');
+const header = document.querySelector('.header');
+const menuLinks = document.querySelectorAll('.header__menu-link');
+
+toggleButton.addEventListener('click', () => {
+    header.classList.toggle('menu-abierto');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        header.classList.remove('menu-abierto');
+    });
+});
+
+// =====================================
+// SEGURO PARA EL RESIZE DE VENTANA
+// =====================================
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        header.classList.remove('menu-abierto');
+    }
+});
